43bc1a8d2e389bbba58cb8f17b076336f64f1b42
Arda Uzunoğlu <ardauzunogluarda@gmail.com>
2021-01-30 22:12:29 +0300

    Add files via upload

--- blog.py
+++ blog.py
@@ -3,8 +3,34 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    sozluk = {"adi":"Tübitak Projesi", "gelistirici":"Arda Uzunoğlu", "konu":"Doğal Dil İşleme"}
-    return render_template("index.html", dict=sozluk)
+    return render_template("index.html")
 
+@app.route("/ana-sayfa")
+def main_page():
+    return render_template("ana-sayfa.html")
+
+@app.route("/hakkimizda")
+def about():
+    return render_template("about.html")
+
+@app.route("/siir-tahlili")
+def donem_tahmini():
+    return render_template("donem-tahmini.html")
+
+@app.route("/buyuk-unlu-uyumu-kontrolu")
+def buyuk_unlu_uyumu_kontrolu():
+    return render_template("buyuk-unlu-uyumu-kontrolu.html")
+
+@app.route("/gelistirme-asamasinda-olan-ozellikler")
+def gelistirme_asamasinda_olan_ozellikler():
+    return render_template("gelistirme-asamasinda-olan-ozellikler.html")
+
+@app.route("/iletisim")
+def iletisim():
+    return render_template("contact.html")
+
+@app.route("/geri-bildirim-formu")
+def geri_bildirim_formu():
+    return render_template("geri-bildirim-formu.html")
 if __name__ == "__main__":
     app.run(debug=True)
